ef372de6aaf30ca78bf8af192c3b3da589b06145
Taslim Arif <taslimarif600@gmail.com>
2023-03-17 00:46:20 +0530

    Added migration

--- resources/user.py
+++ resources/user.py
@@ -1,16 +1,31 @@
+import os
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+import requests
+from sqlalchemy import or_
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt, create_refresh_token, get_jwt_identity
 
 from config import BLOCKLIST, db
 from middleware.api_key import verify_x_api_key
 from models import UserModel
-from schema import UserSchema
+from schema import UserSchema, UserRegisterSchema
 from passlib.hash import pbkdf2_sha256
 from datetime import timedelta
 UserBlueprint = Blueprint(
     "Users", "users", description="Operations on users")
+
+
+def send_simple_message(to, subject, body):
+    return requests.post(
+        f"https://api.mailgun.net/v3/{os.getenv('MAILGUN_DOMAIN')}/messages",
+        auth=("api", f"{os.getenv('MAILGUN_API_KEY')}"),
+        data={
+            "from": f"Taslim Arif <postmaster@{os.getenv('MAILGUN_DOMAIN')}>",
+            "to": [to],
+            "subject": subject,
+            "text": body
+        })
 
 
 @UserBlueprint.route("/user/<string:user_id>")
@@ -52,15 +67,25 @@ class User(MethodView):
 class UserRegister(MethodView):
 
     # @verify_x_api_key
-    @UserBlueprint.arguments(UserSchema)
-    @UserBlueprint.response(201, UserSchema)
+    @UserBlueprint.arguments(UserRegisterSchema)
+    @UserBlueprint.response(201, UserRegisterSchema)
     def post(self, user_data):
         try:
+            if UserModel.query.filter(
+                or_(UserModel.username == user_data['username'],
+                    UserModel.email == user_data['email'])
+            ).first():
+                abort(409, message='A user with that username or email already exists')
             username = user_data['username']
             password = pbkdf2_sha256.hash(user_data['password'])
             user = UserModel(username=username, password=password)
             db.session.add(user)
             db.session.commit()
+            send_simple_message(
+                to=user.email,
+                subject='Successfully signed up',
+                body=f'Hi {user.username}! You have successfully signed up to Stores API.'
+            )
             return user
         except IntegrityError:
             abort(
--- schema/__init__.py
+++ schema/__init__.py
@@ -1,4 +1,4 @@
 from schema.item_schema import ItemSchema, ItemUpdateSchema
 from schema.store_schema import StoreSchema
 from schema.tag_schema import TagAndItemSchema, TagSchema
-from schema.user_schema import UserSchema
+from schema.user_schema import UserSchema, UserRegisterSchema
